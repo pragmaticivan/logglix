de91f0fb701cdb10f156f592226224ae82bd9925
Ivan Santos <pragmaticivan@gmail.com>
2017-02-24 00:37:10 -0800

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Logglix.Mixfile do
 
   def project do
     [app: :logglix,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
